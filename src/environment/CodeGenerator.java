74309bcf64eb5c7cef0068cd383799b85dd587ea
dawid <talagadawid@gmail.com>
2016-04-27 16:30:55 +0200

    1-4 gotowe

---
+++ src/environment/CodeGenerator.java
@@ -0,0 +1,23 @@
+package environment;
+
+/**
+ * Created by dawid on 27.04.16.
+ */
+public class CodeGenerator {
+    static Node tree;
+
+//    public static void searchTree(Node tree){
+//        if(tree.getLeft() == null && tree.getRight()==null)
+//            System.out.print(tree.getToken() + " ");
+//        else{
+//            if(tree.getLeft()==null)
+//            {
+//                searchTree(tree.getRight());
+//            }
+//            else{
+//                searchTree(tree.getLeft());
+//            }
+//
+//        }
+//    }
+}
---
+++ src/environment/Node.java
@@ -0,0 +1,34 @@
+package environment;
+
+public class Node{
+    String token;
+    Node left;
+    Node right;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+
+    //gettery settery
+}
